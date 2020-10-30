f95faf5471da7c642bd55f0e81313da1672bf372
morenikeji <ajisegirimorenikeji@gmail.com>
2020-10-30 07:44:59 -0700

    update component

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import Customer from './Customer'
+import axios from 'axios'
+
+class App extends Component{
+
+  state = {
+    orders: []
+  }
+
+  componentDidMount(){
+    axios.get('https:indapi.kumba.io/webdev/assignment')
+      .then( res => {
+        this.setState({
+          orders : res.data
+        })
+      })
+  }
+render(){
+  const { orders } = this.state;
+  const orderList = orders.items ? 
+    (orders.items.map( (order, i) => {
+            return(
+              <div className='order' key={i}>
+                 <div className='order-content'>
+                   <h1 className='order-name'>{order.name}</h1>
+                   <div className='order-name'>CATEGORY: {order.category}</div>
+                   <div className='order-name'>PRICE: {order.currency} {order.price}</div>
+                   <div className='order-name'>TAX: {order.tax_pct} % </div>
+                   <div className='order-name'>QUANTITY: {order.quantity}</div>
+                 </div>
+                  </div>)
+            })):
+  
+    <div className='center'> loading ....</div> 
+
+  return (
+  <div className="container">
+     <nav>
+        <div className='navbar'>Fast Food Station</div>
+      </nav>
+      <div>
+      {orders.user &&  <Customer user ={orders.user }/>}
+      </div>
+      <div className='items'>{orderList}</div>
+      <div className='footer-container'>
+      <div className='footer-heading'>{orders.restaurant && orders.restaurant.name }</div>
+      <div className='footer-state'>{orders.restaurant && orders.restaurant.state},{orders.restaurant && orders.restaurant.city}</div>
+      <div className='footer-street'>{orders.restaurant && orders.restaurant.street },{orders.restaurant && orders.restaurant.zipcode}.</div>
+      </div>
+    </div>
+  )
+ }
+}
+
+export default App;
---
+++ src/Customer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import user from './image/user.jpg'
+
+
+function Customer(props) {
+    
+   return (
+        <div className='user-container'>
+            <div className='user'>
+                <div className='pics'>
+                    <img src={user} className='profile-pics' alt='profile pics'/>
+                </div>
+               <div className='user-info'>
+                   <div className='data'>
+                 <p><h4>NAME:</h4>{props.user.name}</p>
+                 <p><h4>CONTACT:</h4> {props.user.phone}</p>
+                 <p><h4>ADDRESS:</h4> {props.user.address}</p>
+                 <p><h4>ABOUT: </h4>{props.user.about}</p>
+                 </div>
+                 <div className='user-likes'>
+                    <div className='likes'>
+                    <h4>LIKE:</h4> 
+                    <p>{ props.user.likes.map(like => {
+
+                   return  <h4 className='like'>{like}</h4>
+
+
+                     })}</p>
+                    </div>
+
+                    <div className='likes'>
+                   <h4>DISLIKE:</h4> 
+                   <p>{props.user.dislikes.map(dislike => {
+
+                     return <h4  className='like'>{dislike}</h4>
+
+                   })}</p>
+                   </div>
+                 </div>
+               </div>
+            </div>
+             
+        </div>
+    )
+   }
+
+export default Customer
